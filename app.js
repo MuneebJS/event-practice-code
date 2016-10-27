ae0eeaddceeb0ed07d12efa2bbdfc7ea6386ab71
Muneeb Khan <Muneeb Khan>
2016-10-27 00:03:14 -0700

    practice is continue

---
+++ app.js
@@ -0,0 +1,57 @@
+// function expand() {
+//     var more = "I enjoy whatever I do thanks";
+//     document.getElementById("more").innerHTML = more;
+// }
+
+// function showAlert() {
+//     var hy = document.getElementById("para").innerHTML;
+//     alert(hy);
+// }
+
+// function showList() {
+//     var list = "<ul><li>Karachi</li><li>Lahore</li><li>Quetta</li></ul>"
+//     document.getElementById("show").innerHTML = list;
+    // document.getElementById("hide").className = "hidden";
+// }
+
+// function replace() {
+//     document.getElementById("ugly").className = "hidden";
+// }
+
+// function swapImage (id, newPic) {
+//     document.getElementById(id).src = newPic;    
+// }
+
+// function change(id1, picName) {
+//     var link = document.getElementById(id1);
+//     link.src = picName; 
+// } {}
+// function getLink() {
+//     var link = document.getElementById("yes")
+//     var addClass = link.className = " world";
+//     var address = link.className; 
+//     alert(address);
+// }
+
+// function addClass() {
+//     var href = document.getElementById("yes")
+//     href.className += " world";
+//     alert(href.className);
+// }
+
+// var m = document.getComputedStyle("yes").margin;
+
+// function get() {
+// // var m = document.getComputedStyle("yes").margin;
+// alert("ms");
+// 
+
+// function hello(size, margin) {
+//     document.getElementById('yes').style.fontSize = size;
+//     document.getElementById("yes").style.margin = margin;
+// }
+
+// function showAlert() {
+//     var get = document.getElementById('yes').style.margin;
+//     alert(get);
+// }
